6f51a9acf47898be8132b4d43898f3608dee28c1
Denise Nordfalt <denisenordfalt@gmail.com>
2022-09-14 18:44:27 +0200

    added head for title

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,11 @@
+import Head from "next/head";
+
 const Layout = ({ children }) => (
   <div>
+    <Head>
+      <title>Denise CMS</title>
+    </Head>
+
     <div className="max-w-5xl p-10 mx-auto">{children}</div>
   </div>
 );
