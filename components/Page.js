71e2a75295ba6d10f9f4ae4f71b0e3db122cc52d
Denise Nordfalt <denisenordfalt@gmail.com>
2022-09-16 00:44:51 +0200

    added conditional chaining

--- components/Page.js
+++ components/Page.js
@@ -2,9 +2,10 @@ import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 
 const Page = ({ blok }) => (
   <main className="px-6" {...storyblokEditable(blok)} key={blok._uid}>
-    {blok.body.map((nestedBlok) => (
-      <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-    ))}
+    {blok?.body &&
+      blok.body.map((nestedBlok) => (
+        <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+      ))}
   </main>
 );
 
