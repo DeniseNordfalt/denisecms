ddedcfc6234e85bd3988d4a3759451973f5467ec
Denise Nordfalt <denisenordfalt@gmail.com>
2022-09-16 00:45:33 +0200

    added small menu and favicon to layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,30 @@
 import Head from "next/head";
+import Footer from "./Footer";
+import Link from "next/link";
 
-const Layout = ({ children }) => (
-  <div>
-    <Head>
-      <title>Denise CMS</title>
-    </Head>
+export default function Layout({ children, global }) {
+  console.log("global", global?.navigation);
 
-    <div className="max-w-5xl p-10 mx-auto">{children}</div>
-  </div>
-);
+  return (
+    <div>
+      <Head>
+        <title>Denise CMS</title>
+        <link rel="icon" href={global?.favicon?.filename} />
+      </Head>
 
-export default Layout;
+      {global?.navigation &&
+        global?.navigation?.map((nav_link, index) => {
+          return (
+            <div key={index}>
+              <Link href={nav_link.link.cached_url}>
+                <a>{nav_link.name}</a>
+              </Link>
+            </div>
+          );
+        })}
+
+      <div className="max-w-5xl p-10 mx-auto">{children}</div>
+      <Footer />
+    </div>
+  );
+}
